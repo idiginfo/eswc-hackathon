6443c0b4ce6d4dfb5554b44fdfd9117e8a546ed7
root <root@idiginfo-scholrev.novalocal>
2013-04-23 14:24:28 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+	<title>Jailbreaking the PDF Hackathon</title>
+	<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/js.js"></script>
+	<script type="text/javascript" src="js/jquery.stellar.min.js"></script>
+	<script type="text/javascript" src="js/waypoints.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+</head>
+
+<body>
+
+<div class="menu">	
+	<div class="container clearfix">
+
+		<div id="logo" class="grid_6">
+			<a href='http://2013.eswc-conferences.org/' title='Extended Sematic Web Conference 2013'><img src="img/logo_eswc13.jpg"></a>
+			<a href='https://groups.google.com/forum/?fromgroups#!forum/scholrev-mailing-list' title='ScholRev Community'><img src="img/logo_scholrev.jpg"></a>
+			<a href='http://idiginfo.org' title='FSU Institute for Digital Information'><img src="img/logo_idiginfo.jpg"></a>
+			<a href='http://www.fsu.edu' title='Florida State University'><img src="img/logo_fsu.jpg"></a>
+		</div>
+		
+		<div id="nav" class="grid_6 omega">
+				<div id="big-button"><a href="http://wehack.it/hackathons/47-jailbreaking-the-pdf">Sign Up</a></div>
+			<ul class="navigation">
+				<li data-slide="1">Overview</li>
+				<li data-slide="2">Schedule</li>
+				<li data-slide="4">Ideas</li>
+			</ul>
+		</div>
+	
+	</div>
+</div>
+
+
+<div class="slide" id="slide1" data-slide="1" data-stellar-background-ratio="0.5">
+	<div class="container clearfix">
+		
+		<div id="content" class="grid_12">
+			<h1>Jailbreaking the PDF: a Hackathon</h1>
+			<h2>Monday, May 27 in Montpellier, France</h2>
+		</div>
+		<div class="grid_6 alpha">
+		    <p><strong>Join us in Montpellier for a one-day event to hack on scholarly PDFs!</strong></p>
+		    <p>Currently, the bulk of peer-reviewed scientific knowledge is locked up in PDF documents, which are difficult to get information .</p>
+		    <p>We want to change that.</p>
+		    <p>If you’re interested in hacking on PDFs and exploring ways to access scholarly data in modern ways, this hackathon is for you.</p>
+        </div>
+        
+        <div class="grid_6 omega">
+            
+
+        </div>
+		
+		
+	
+	</div>
+</div>
+
+
+
+<div class="slide" id="slide2" data-slide="2" data-stellar-background-ratio="0.5">
+	<div class="container clearfix">
+		
+		<div id="content2" class="grid_12">
+        	<h1>Schedule (tentative)</h1>
+			<h2>Will be held at the <a href='http://2013.eswc-conferences.org/' title='EWSC Homepage'>Extended Semantic Web Conference</a>
+			<br />Monday, May 27, 2013, Montpellier, France</h2>
+		</div>
+		
+		<div class="grid_6">
+		<ul>
+	        <li>8am - Coffee and breakfast arrive</li>
+	        <li>9am - Hackathon begins - Discuss logistics</li>
+	        <li>12:00pm - Lunch arrives</li>
+	        <li>5:00pm - Presentation of projects</li>
+        </ul>
+        </div> <div class="grid_6 omega">
+        <h4>Perks</h4>
+        <ul>
+	        <li>All the Soda, coffee, and beer you can drink</li>
+	        <li>Vegan and non-vegan food provided - Breakfast and lunch</li>
+	        <li>Wired and wireless network access</li>
+        </ul>
+        <h4>Attending Remotely</h4>
+        <p>We will have a Google Hangout feed going.  Stay tuned for the URL.</p>
+        
+        
+		</div>
+	
+	</div>
+</div>
+
+
+
+<div class="slide" id="slide3" data-slide="3" data-stellar-background-ratio="0.5">
+	<div class="container clearfix">
+		
+		<div id="content3" class="grid_12">
+		
+
+        </div>
+	
+	</div>
+</div>
+
+
+
+<div class="slide" id="slide4" data-slide="4" data-stellar-background-ratio="0.5">
+	<div class="container clearfix">
+		
+		<div id="content" class="grid_12">
+			<h1>Tools and Ideas</h1>
+			<h2>Existing Tools to Hack On or Use (if you want)</h2>
+	    <div class="grid_6">		
+        <h4>PDF2SVG</h4>
+        <ul>
+            <li>A JAVA Library for converting PDFs to SVG form</li>
+            <li><a href="https://bitbucket.org/petermr/pdf2svg-dev" target-"_blank">https://bitbucket.org/petermr/pdf2svg-dev</a></li>
+        </ul>
+        <h4>PDFExtract</h4>
+        <ul>
+            <li>A tool by CrossRef that attempts to extract citations from a PDF</li>
+            <li><a href="https://github.com/CrossRef/pdfextract" target="_blank">https://github.com/CrossRef/pdfextract</a></li>
+        </ul>
+        <h4>Poppler PDF Tools</h4>
+        <ul>
+            <li>A basic PDF to text tool</li>
+            <li><a href="http://poppler.freedesktop.org/" target="_blank">http://poppler.freedesktop.org/</a></li>
+        </ul>
+        <h4>PDFMiner</h4>
+        <ul>
+            <li>Python libraries for converting PDF to text or to XML</li>
+            <li><a href="http://www.unixuser.org/~euske/python/pdfminer/" target="_blank">http://www.unixuser.org/~euske/python/pdfminer/</a></li>
+        </ul>
+        <h4>Python NLTK</h4>
+        <ul>
+            <li>Natural Language Toolkit for Python</li>
+            <li><a href="http://nltk.org/book/" target="_blank">http://nltk.org/book/</a></li>
+        </ul>
+        <h4>PDFX</h4>
+        <ul>
+            <li>Scholarly PD extraction web service endpoint from our friends at Utopia (http://getutopia.com/)</li>
+            <li><a href="http://pdfx.cs.man.ac.uk/" target="_blank">http://pdfx.cs.man.ac.uk/</a></li>
+        </ul>
+        <h4>ParsCit</h4>
+        <ul>
+            <li>A web service endpoint and Ruby library for parsing reference strings and logical document structure</li>
+            <li><a href="http://aye.comp.nus.edu.sg/parsCit/" target="_blank">http://aye.comp.nus.edu.sg/parsCit/</a></li>
+        </ul>
+        <h4>Grobid</h4>
+        <ul>
+            <li>A JAVA library for extracting all kinds of scholarly information from text</li>
+            <li><a href="https://github.com/grobid/grobid" target="_blank">https://github.com/grobid/grobid</a></li>
+        </ul>
+        <h4>Biblio::Citation::Parser</h4>
+        <ul>
+            <li>A Perl Package for parsing citations from text</li>
+            <li><a href="http://cpansearch.perl.org/src/MJEWELL/Biblio-Citation-Parser-1.10/docs/html/refparsing.html" target="_blank">http://cpansearch.perl.org/src/MJEWELL/Biblio-Citation-Parser-1.10/docs/html/refparsing.html</a></li>
+        </ul>
+
+
+        </div> <div class="grid_6 omega">
+        <h4>Hacking Ideas</h4>
+
+        <ul class='ideas'>
+	        <li>Improve automatic identification of citation references in a PDF and extract them into structured markup</li>
+            <li>Identify the main narrative in a PDF and extract it into structured markup</li>
+            <li>Identify passages within text that correspond to a footnote or reference at the end of a document</li>
+            <li>Computationally determine the level of accuracy that has been achieved by attempting to do the preceding three things.</li>
+        </ul>
+
+
+        <h4>Further Reading and Watching</h4>
+        <ul>
+	        <li><a href="http://www.force11.org/" target="_blank">http://www.force11.org/</a></li>
+	        <li><a href="http://2013.eswc-conferences.org/" target="_blank">http://2013.eswc-conferences.org/</a></li>
+	        <li><a href="https://www.youtube.com/watch?v=6jf2PZDmuK4" target="_blank">https://www.youtube.com/watch?v=6jf2PZDmuK4</a></li>
+        </ul>
+
+
+        </div>
+        
+        
+		</div>
+	
+	</div>
+</div>
+
+
+</body>
+</html>

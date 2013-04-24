da40e668434bcdb75ad8f0b44e7cbfc0c28cae8f
Alex Constantin <aconstantin@cs.man.ac.uk>
2013-04-24 10:59:36 +0100

    Added more PDF tools/libraries

--- index.php
+++ index.php
@@ -120,19 +120,14 @@
 			<h1>Tools and Ideas</h1>
 			<h2>Existing Tools to Hack On or Use (if you want)</h2>
 	    <div class="grid_6">		
-        <h4>PDF2SVG</h4>
+        <h4>xpdf</h4>
         <ul>
-            <li>A JAVA Library for converting PDFs to SVG form</li>
-            <li><a href="https://bitbucket.org/petermr/pdf2svg-dev" target-"_blank">https://bitbucket.org/petermr/pdf2svg-dev</a></li>
+            <li>Open-source PDF viewer</li>
+            <li><a href="http://www.foolabs.com/xpdf/" target="_blank">http://www.foolabs.com/xpdf/</a></li>
         </ul>
-        <h4>PDFExtract</h4>
+        <h4>poppler PDF tools</h4>
         <ul>
-            <li>A tool by CrossRef that attempts to extract citations from a PDF</li>
-            <li><a href="https://github.com/CrossRef/pdfextract" target="_blank">https://github.com/CrossRef/pdfextract</a></li>
-        </ul>
-        <h4>Poppler PDF Tools</h4>
-        <ul>
-            <li>A basic PDF to text tool</li>
+            <li>PDF rendering library based on xpdf</li>
             <li><a href="http://poppler.freedesktop.org/" target="_blank">http://poppler.freedesktop.org/</a></li>
         </ul>
         <h4>PDFMiner</h4>
@@ -140,32 +135,76 @@
             <li>Python libraries for converting PDF to text or to XML</li>
             <li><a href="http://www.unixuser.org/~euske/python/pdfminer/" target="_blank">http://www.unixuser.org/~euske/python/pdfminer/</a></li>
         </ul>
-        <h4>Python NLTK</h4>
+        <h4>PDFBox</h4>
         <ul>
-            <li>Natural Language Toolkit for Python</li>
-            <li><a href="http://nltk.org/book/" target="_blank">http://nltk.org/book/</a></li>
+            <li>An open-source Java PDF Library</li>
+            <li><a href="http://pdfbox.apache.org/" target="_blank">http://pdfbox.apache.org/</a></li>
+        </ul>
+        <h4>PDF2SVG</h4>
+        <ul>
+            <li>A Java Library for converting PDFs to SVG form based on PDFBox</li>
+            <li><a href="https://bitbucket.org/petermr/pdf2svg-dev" target-"_blank">https://bitbucket.org/petermr/pdf2svg-dev</a></li>
+        </ul>
+        <h4>PDFExtract</h4>
+        <ul>
+            <li>A Java toolkit for text and structure extraction from PDFs based on PDFBox</li>
+            <li><a href="https://github.com/elacin/PDFExtract" target="_blank">https://github.com/elacin/PDFExtract</a></li>
+        </ul>
+        <h4>pdf-extract</h4>
+        <ul>
+            <li>A tool by CrossRef that attempts to extract references from PDF articles</li>
+            <li><a href="https://github.com/CrossRef/pdfextract" target="_blank">https://github.com/CrossRef/pdfextract</a></li>
         </ul>
         <h4>PDFX</h4>
         <ul>
-            <li>Scholarly PD extraction web service endpoint from our friends at Utopia (http://getutopia.com/)</li>
+            <li>Scholarly PDF-to-XML conversion web service endpoint from our friends at Utopia (http://getutopia.com/)</li>
             <li><a href="http://pdfx.cs.man.ac.uk/" target="_blank">http://pdfx.cs.man.ac.uk/</a></li>
         </ul>
-        <h4>ParsCit</h4>
+        <h4>ParsCit + SectLabel</h4>
         <ul>
-            <li>A web service endpoint and Ruby library for parsing reference strings and logical document structure</li>
+            <li>A web service endpoint and Ruby libraries for parsing reference strings and logical document structure</li>
             <li><a href="http://aye.comp.nus.edu.sg/parsCit/" target="_blank">http://aye.comp.nus.edu.sg/parsCit/</a></li>
+        </ul>
+        <h4>pdf2xml (1)</h4>
+        <ul>
+            <li>PDF-to-geometrical-XML convertor based on xpdf</li>
+            <li><a href="http://sourceforge.net/projects/pdf2xml/" target="_blank">http://sourceforge.net/projects/pdf2xml/</a></li>
         </ul>
         <h4>Grobid</h4>
         <ul>
-            <li>A JAVA library for extracting all kinds of scholarly information from text</li>
+            <li>A JAVA machine learning library for extracting scholarly information based on pdf2xml (1)</li>
             <li><a href="https://github.com/grobid/grobid" target="_blank">https://github.com/grobid/grobid</a></li>
+        </ul>
+        <h4>pdf2xml (2)</h4>
+        <ul>
+            <li>another PDF-to-geometrical-XML project based on xpdf</li>
+            <li><a href="http://www.mobipocket.com/dev/pdf2xml/" target="_blank">http://www.mobipocket.com/dev/pdf2xml/</a></li>
+        </ul>
+        <h4>JPedal</h4>
+        <ul>
+            <li>LGPL version of the Java PDF library of http://www.idrsolutions.com/</li>
+            <li><a href="http://sourceforge.net/projects/jpedal/" target="_blank">http://sourceforge.net/projects/jpedal/</a></li>
+        </ul>
+        <h4>LA-PDFText</h4>
+        <ul>
+            <li>Layout-aware PDF text extractor based on JPedal and DROOLS rule sets</li>
+            <li><a href="http://code.google.com/p/lapdftext/" target="_blank">http://code.google.com/p/lapdftext/</a></li>
+        </ul>
+        <h4>pdf2htmlEX</h4>
+        <ul>
+            <li>Layout-preserving PDF-to-HTML converter based on poppler</li>
+            <li><a href="http://coolwanglu.github.io/pdf2htmlEX/" target="_blank">http://coolwanglu.github.io/pdf2htmlEX/</a></li>
         </ul>
         <h4>Biblio::Citation::Parser</h4>
         <ul>
             <li>A Perl Package for parsing citations from text</li>
             <li><a href="http://cpansearch.perl.org/src/MJEWELL/Biblio-Citation-Parser-1.10/docs/html/refparsing.html" target="_blank">http://cpansearch.perl.org/src/MJEWELL/Biblio-Citation-Parser-1.10/docs/html/refparsing.html</a></li>
         </ul>
-
+        <h4>Python NLTK</h4>
+        <ul>
+            <li>Natural Language Toolkit for Python</li>
+            <li><a href="http://nltk.org/book/" target="_blank">http://nltk.org/book/</a></li>
+        </ul>
 
         </div> <div class="grid_6 omega">
         <h4>Hacking Ideas</h4>
